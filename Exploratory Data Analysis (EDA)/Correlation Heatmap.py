76fc6036bc9be49e6e1363a242032271929fca7b
ISAAC RITHARSON P <isaacritharson@karunya.edu.in>
2023-07-11 21:33:46 +0530

    Update Correlation Heatmap.py

--- Exploratory Data Analysis (EDA)/Correlation Heatmap.py
+++ Exploratory Data Analysis (EDA)/Correlation Heatmap.py
@@ -1,6 +1,6 @@
 # Employee Table
 corr = t.corr()
-corr.style.background_gradient(cmap='coolwarm')
+corr.style.background_gradient(cmap='coolwarm'
 # Titanic Dataset
 corr = df.corr()
 corr.style.background_gradient(cmap='coolwarm')
