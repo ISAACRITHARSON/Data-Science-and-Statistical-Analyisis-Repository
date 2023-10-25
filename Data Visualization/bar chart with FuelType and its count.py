9c1a4130fb60b96066c6a56fe041c1721d429f44
ISAAC RITHARSON P <isaacritharson@karunya.edu.in>
2023-10-25 23:25:25 +0530

    Update bar chart with FuelType and its count.py

--- Data Visualization/bar chart with FuelType and its count.py
+++ Data Visualization/bar chart with FuelType and its count.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-import pandas as pd
+import pandas as p
 df = pd.read_excel('/content/Datavisualization.xlsx')
 df['FuelType'].value_counts().plot(kind="bar",xlabel='Fuel
 type',ylabel='Count')
